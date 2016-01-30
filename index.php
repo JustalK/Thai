67d57445d3942d87c8ff9a11b27abbb38681add4
Kevin Justal <kevin.justal@etu.unice.fr>
2016-01-30 16:54:59 +0100

    [1] Starting the header

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+	<title>Insert title here</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+	<!-- Framework -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row header-top-top">
+			<div class="col-md-offset-2 col-md-1 max-height ">
+				<img src="imgs/unnamed.png" class="img-responsive-custom">
+			</div>
+			<div class="col-md-1 header-top-button">
+				Alphabet
+			</div>
+			<div class="col-md-1 header-top-button">
+				Le&ccedil;ons
+			</div>
+		</div>
+		
+		<div class="row header-top-middle">
+			<div id="logo" style="height: 100%;">
+				<div class="col-md-offset-2 col-md-2 max-height" style="text-align: center;line-height: 400px;">
+					TITLE
+				</div>
+				<div class="col-md-2 max-height" style="text-align: center;line-height: 400px;">
+					TITLE
+				</div>
+			</div>
+		</div>
+		
+		<div class="row header-top-bottom" >
+			<div class="col-md-12">
+				azeazeae
+			</div>
+		</div>		
+	</div>
+</body>
+</html>
