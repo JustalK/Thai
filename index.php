ab82fd5f2298fbcf8ba28b505312f79484279987
Kevin Justal <kevin.justal@etu.unice.fr>
2016-01-31 01:16:02 +0100

    [2] Making some new design

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Insert title here</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" media="screen" rel="stylesheet" type="text/css" />
 
 	<!-- Framework -->
 	<script src="js/jquery.min.js"></script>
@@ -13,31 +14,37 @@
 <body>
 	<div class="container-fluid">
 		<div class="row header-top-top">
-			<div class="col-md-offset-2 col-md-1 max-height ">
+			<div class="col-md-offset-2 col-md-1 max-height">
 				<img src="imgs/unnamed.png" class="img-responsive-custom">
 			</div>
 			<div class="col-md-1 header-top-button">
-				Alphabet
+				<a>Alphabet</a>
 			</div>
 			<div class="col-md-1 header-top-button">
-				Le&ccedil;ons
+				<a>Le&ccedil;ons</a>
 			</div>
+			<div class="col-md-offset-4 col-md-1">
+				<div id="login">Login</div>
+			</div></div>
 		</div>
 		
 		<div class="row header-top-middle">
 			<div id="logo" style="height: 100%;">
-				<div class="col-md-offset-2 col-md-2 max-height" style="text-align: center;line-height: 400px;">
-					TITLE
+				<div id="alphabet" class="col-md-offset-2 col-md-2 max-height">
+					 <div id="circle"></div>
 				</div>
-				<div class="col-md-2 max-height" style="text-align: center;line-height: 400px;">
-					TITLE
+				<div id="title" class="col-md-8 max-height" style="float: left;display: flex;align-items: center;">
+					<div style="max-width: 100%;">
+						<h1 style="font-weight: 300 !important;">Apprendre le thailandais</h1>
+						<div class="myButton">Start</div>
+					</div>
 				</div>
 			</div>
 		</div>
 		
 		<div class="row header-top-bottom" >
-			<div class="col-md-12">
-				azeazeae
+			<div class="col-md-offset-2 col-md-1 max-height">
+				<a class="path">Home</a> > <strong>Alphabet</strong>
 			</div>
 		</div>		
 	</div>
