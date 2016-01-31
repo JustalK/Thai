3892e2a87a64aa8f2cc487796f039d4ec78c33b6
Kevin Justal <kevin.justal@etu.unice.fr>
2016-01-31 17:24:53 +0100

    [4] Adding the adaptability

--- index.php
+++ index.php
@@ -14,26 +14,27 @@
 <body>
 	<div class="container-fluid">
 		<div class="row header-top-top">
-			<div class="col-md-offset-2 col-md-1 max-height">
+			<div class="col-md-offset-2 col-md-1 col-sm-offset-2 col-sm-2 hidden-xs max-height">
 				<img src="imgs/unnamed.png" class="img-responsive-custom">
 			</div>
-			<div class="col-md-1 header-top-button">
+			<div class="col-md-1 col-sm-1 col-md-offset-0 col-sm-offset-0 col-xs-offset-1 col-xs-2 header-top-button">
 				<a>Alphabet</a>
 			</div>
-			<div class="col-md-1 header-top-button">
+			<div class="col-md-1 col-sm-1 col-xs-2 header-top-button">
 				<a>Le&ccedil;ons</a>
 			</div>
-			<div class="col-md-offset-4 col-md-1">
+			<div class="col-md-offset-3 col-md-2 col-sm-offset-3 col-sm-2 col-xs-offset-3 col-xs-2">
 				<div id="login">Login</div>
-			</div></div>
+			</div>
 		</div>
 		
 		<div class="row header-top-middle">
 			<div id="logo" style="height: 100%;">
-				<div class="col-md-offset-2 col-md-2 col-sm-2 max-height" style="">
-					<img src="imgs/circle.png" class="img-responsive" style="position: relative;top:50%;transform: translateY(-50%);" />
+				<div class="col-md-offset-2 col-md-2 col-sm-offset-1 col-sm-4 hidden-xs max-height" style="position:relative;">
+					<img src="imgs/alphabet.png" class="img-responsive circle"/>
+					<img src="imgs/circle.png" class="img-responsive circle" />
 				</div>
-				<div id="title" class="col-md-8 max-height" style="float: left;display: flex;align-items: center;">
+				<div id="title" class="col-md-8 col-md-offset-0 col-sm-offset-0 col-xs-offset-1 max-height" style="float: left;display: flex;align-items: center;">
 					<div style="max-width: 100%;">
 						<h1 style="font-weight: 300 !important;">Apprendre le thailandais</h1>
 						<div class="myButton">Start</div>
@@ -43,7 +44,42 @@
 		</div>
 		
 		<div class="row header-top-bottom" >
-			<div class="col-md-offset-2 col-md-1 max-height">
+			<div class="col-md-offset-2 col-md-10 col-sm-offset-2 col-sm-10 col-xs-offset-1 col-xs-10 max-height">
+				<a class="path">Home</a> > <strong>Alphabet</strong>
+			</div>
+		</div>	
+		
+		<div class="row" >
+			<div class="col-md-offset-2 col-md-5 max-height">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-12" style="padding-left: 0px;padding-right: 0px;margin-top:20px;">
+							<p class="gray">RESUME ET DESCRIPTION</p><br /><br />
+							<p>Voici les differents elements qui m'ont permis d'apprendre l'alphabet thailandais.
+							L'alphabet thailandais, bien que rebutant au depart, est plus facile qu'il n'y parait. 
+							La grosse et unique difficulte reside dans la differanciation entre les tonalites et la longueur des sons.
+							N'etant pas musicien, il m'a fallu deux mois pour retenir de maniere parfaite les differents sons.
+							</p>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-md-12" style="padding-left: 0px;padding-right: 0px;margin-top:10px;">
+							<p class="gray">LE&Ccedil;ONS</p><br /><br />
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-md-12" style="padding-left: 0px;padding-right: 0px;margin-top:10px;">
+							<p class="gray">Les premieres consonnes</p><br /><br />
+							<p>Les premiers sons 
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-5">
 				<a class="path">Home</a> > <strong>Alphabet</strong>
 			</div>
 		</div>		
